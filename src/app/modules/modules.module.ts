e49264d483b8b2617510ee0a1f7fd64db0f7be0d
lourdesaa <loudaniele28@gmail.com>
2024-06-19 11:05:33 -0300

    empiezo proyecto evaluativo

---
+++ src/app/modules/modules.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComponentesComponent } from './componentes/componentes.component';
+
+
+
+@NgModule({
+  declarations: [
+    ComponentesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ModulesModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyB4n0a-d3aqk3CSH4kZWmcwj-Uku6O2aII",
+        authDomain: "proyectotp-f5429.firebaseapp.com",
+        projectId: "proyectotp-f5429",
+        storageBucket: "proyectotp-f5429.appspot.com",
+        messagingSenderId: "168089627497",
+        appId: "1:168089627497:web:16c2506de00d102b196bf8"
+    }
+}
