58c4f420c1e2bff6fd3d514d3a47a3f25e9d04cf
lourdesaa <loudaniele28@gmail.com>
2024-07-04 10:54:10 -0300

    use @mediaquerys, y cree componentes de footer y navbar

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './component/footer/footer.component';
+import { NavbarComponent } from './component/navbar/navbar/navbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterComponent,
+    NavbarComponent
+  ],
   imports: [
     CommonModule
   ]
