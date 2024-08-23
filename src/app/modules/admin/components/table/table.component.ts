87ecf3ae8b9a5520f96ae1e5e6440905736f6e41
lourdesaa <loudaniele28@gmail.com>
2024-08-23 11:06:14 -0300

    cree componentes 'card-' para cada genero de peliculas, me falta arreglar un error pero bueno jiji

---
+++ src/app/modules/admin/components/table/table.component.ts
@@ -0,0 +1,123 @@
+import { Component } from '@angular/core';
+import { Producto } from 'src/app/models/producto'
+import { CrudService } from '../../services/crud.service';
+import { FormControl, FormGroup, Validator, Validators } from '@angular/forms';
+import { Subscriber } from 'rxjs';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent {
+  //crear cleccion de productos del tipo producto -> la definimos como un array
+  coleccionProductos: Producto[] = []
+
+  //para manejar el estado de edicion y eliminacion de productos
+  modalVisibleProducto: boolean = false;
+
+  //va a tomar el producto que nosotros elijamos
+  productoSeleccionado!: Producto; //recibe valores vacios
+
+
+  //definimos formulario para los productos
+  //atributos alfanumericos (string) se inicializan con comillas simples
+  //atributos numericos (number) se inicializan con cero "0"
+  producto = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    precio: new FormControl(0, Validators.required),
+    descripcion: new FormControl('', Validators.required),
+    categoria: new FormControl('', Validators.required),
+    imagen: new FormControl('', Validators.required),
+    alt: new FormControl('', Validators.required),
+  })
+
+  constructor(public servicioCrud: CrudService) { }
+
+  ngOnInit(): void {
+    // subscribe => notifica constantemente los cambios actuales del sistema 
+    this.servicioCrud.obtenerProducto().subscribe(producto => {
+      // guarda la notificacion recibida como un nuevo "producto" a la coleccion
+      this.coleccionProductos = producto;
+    })
+  }
+
+  async agregarProducto() {
+    if (this.producto.valid) {
+      let nuevoProducto: Producto = {
+        idProducto: '',
+        nombre: this.producto.value.nombre!,
+        precio: this.producto.value.precio!,
+        descripcion: this.producto.value.descripcion!,
+        categoria: this.producto.value.categoria!,
+        imagen: this.producto.value.imagen!,
+        alt: this.producto.value.alt!
+      }
+
+      await this.servicioCrud.crearProducto(nuevoProducto)
+        .then(producto => {
+          alert("Ha agregado un nuevo producto con exito")
+          // Limpiamos formulario para agregar nuevos productos
+          this.producto.reset();
+        })
+
+        .catch(error => {
+          alert("Hubo problema al agregar un nuevo producto")
+          this.producto.reset()
+        })
+    }
+  }
+
+  mostrarBorrar(productoSeleccionado: Producto) {
+    this.modalVisibleProducto = true
+    //toma los valores del producto elegido
+    this.productoSeleccionado = productoSeleccionado
+  }
+
+  // Función para eliminar definitivamente al producto
+  borrarProducto() {
+    this.servicioCrud.eliminarProducto(this.productoSeleccionado.idProducto)
+      .then(respuesta => {
+        alert("El producto se ha eliminado correctamente.")
+      })
+      .catch(error => {
+        alert("No se ha podido eliminar el producto \n" + error);
+      })
+  }
+
+
+  // Función para seleccionar el producto a editar
+  mostrarEditar(productoSeleccionado: Producto) {
+    this.productoSeleccionado = productoSeleccionado;
+    // Enviar o "setear" los nuevos valores y reasignarlos a las variables
+    // El ID no se vuelve a enviar ni se modifica, por ende no lo llamamos
+    this.producto.setValue({
+      nombre: productoSeleccionado.nombre,
+      precio: productoSeleccionado.precio,
+      descripcion: productoSeleccionado.descripcion,
+      categoria: productoSeleccionado.categoria,
+      imagen: productoSeleccionado.imagen,
+      alt: productoSeleccionado.alt
+    })
+  }
+
+  editarProducto() {
+    let datos: Producto = {
+      // Solo el ID toma y deja igual su valor
+      idProducto: this.productoSeleccionado.idProducto,
+      nombre: this.producto.value.nombre!,
+      precio: this.producto.value.precio!,
+      descripcion: this.producto.value.descripcion!,
+      categoria: this.producto.value.categoria!,
+      imagen: this.producto.value.imagen!,
+      alt: this.producto.value.alt!
+    }
+    this.servicioCrud.modificarProducto(this.productoSeleccionado.idProducto, datos)
+      .then(producto => {
+        alert("El producto fue modificado con éxito.");
+      })
+      .catch(error => {
+        alert("Hubo un problema al modificar el producto.");
+      })
+  }
+}
---
+++ src/app/modules/admin/services/crud.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { Producto } from 'src/app/models/producto';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudService {
+  //definimos coleccion para los productos de la web de los tipo Producto
+  private productoCollection: AngularFirestoreCollection<Producto>
+
+  constructor(private database: AngularFirestore) {
+    //referenciamos collecion productos y sera subida como "Producto" a firebase
+    this.productoCollection = database.collection('producto')
+  }
+
+  //CREAR productos
+  crearProducto(producto: Producto) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        //creamos numero identificatvio para el producto en la base de datos
+        const idProducto = this.database.createId()
+        //asignamos ID creado al atributo idProducto de la interfaz "Producto"
+        producto.idProducto = idProducto
+        //
+        const resultado = await this.productoCollection.doc(idProducto).set(producto)
+        resolve(resultado)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  //OBTENER productos
+  obtenerProducto() {
+    // snapshotChanges -> toma una captura del estado de los datos 
+    // pipe -> funciona como tuberia que retorna el nuevo arreglo de datos
+    // map -> "mapea" o recorre esa nueva informacion
+    // a -> resguarda la nueva informacion y la envia 
+    return this.productoCollection.snapshotChanges().pipe(map(action => action.map(a => a.payload.doc.data())))
+
+  }
+  //EDITAR productos
+  modificarProducto(idProducto: string, nuevaData: Producto) {
+    //accedemos a la coleccion, buscamos por ID y actualizamos la informacion
+    return this.database.collection('producto').doc(idProducto).update(nuevaData)
+
+
+  }
+
+  //ELIMINAR productos
+  eliminarProducto(idProducto: string) {
+    return new Promise((resolve, reject) => {
+      try {
+        const respuesta = this.productoCollection.doc(idProducto).delete()
+        resolve(respuesta)
+      }
+      catch (error) {
+        reject(error)
+      }
+    })
+  }
+}
+
+function resolve(respuesta: Promise<void>) {
+  throw new Error('Function not implemented.');
+}
